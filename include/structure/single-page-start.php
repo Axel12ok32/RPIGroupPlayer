54d68433513c761524c0da6b013d6f11df45e33f
Axel12ok32 <github@alexspeaker.it>
2023-02-01 15:40:05 +0100

    Commit 0.22.1

--- include/structure/single-page-start.php
+++ include/structure/single-page-start.php
@@ -4,7 +4,7 @@
     <?php include "./include/structure/navigation/navbar.php"; ?>
     <div class="rpi_mfaderpage">
         <div class="header">
-            <img src="./img/RpiGroupPlayWHITE.png" alt="Logo">
+            <img src="./img/RpiGroupPlayWHITE.png" alt="Logo" style="width: 315px;">
             <p class="subtitle"><i>Un nuovo modo di ascoltare musica</i></p>
         </div>
         <div class="content">
