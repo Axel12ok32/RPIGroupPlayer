219229927613fab1fa592642dbca16ee064f6591
Alex Speaker <gitlab@alexspeaker.it>
2023-04-19 12:59:09 +0200

    Commit 0.23.3 Beta

--- include/players/video.php
+++ include/players/video.php
@@ -1,8 +1,11 @@
-<?php include "./include/structure/video-page-start.php"; ?>
+<?php
+    include "./include/structure/video-page-start.php";
+    $tvselect = $_GET['tv'] - 1;
+?>
 
 <div class="playervideo" style="position: absolute; bottom: 0; height: calc(100% - 65px); width: 100%;">
-    <video id="my-video" class="video-js vjs-theme-sea" controls preload="auto" style="width: 100%; height: 100%;" data-setup="{}">
-        <source src="https://stream.video.asvstudios.it/webtv/streams/radiocitta105.m3u8" type="application/x-mpegURL" />
+    <video id="my-video" class="video-js vjs-theme-sea" controls preload="auto" style="width: 100%; height: 100%;" poster="<?php echo $tvlist->station[$tvselect]->poster;?>" data-setup="{}" >
+        <source src="<?php echo $tvlist->station[$tvselect]->stream;?>" type="application/x-mpegURL" />
     </video>
 </div>
 
